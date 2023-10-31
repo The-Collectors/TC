a8a8990da6bafe9adba401157cf02f8a360a9332
JS <skyjune0824@gmail.com>
2023-10-31 16:36:12 -0400

    Tab error fixed

--- backend/model.py
+++ backend/model.py
@@ -4,35 +4,35 @@ from fastapi import UploadFile
 from pydantic import BaseModel
 
 class Club(BaseModel):
-  """Hold information about clubs or organizations."""
-  name: str
-  description: str
-  email: str
-  status: bool
-  size: int
-  image: Optional[str] = None
-  tags: Optional[List[str]] = None
+    """Hold information about clubs or organizations."""
+    name: str
+    description: str
+    email: str
+    status: bool
+    size: int
+    image: Optional[str] = None
+    tags: Optional[List[str]] = None
 
 class Event(BaseModel):
-  """Hold Information about events."""
-  clubName: str
-  name: str
-  description: str
-  image: Optional[str] = None
-  location: Optional[str] = None
-  date: datetime
+    """Hold Information about events."""
+    clubName: str
+    name: str
+    description: str
+    image: Optional[str] = None
+    location: Optional[str] = None
+    date: datetime
   
 class User(BaseModel):
-  """Hold Information about Users."""
-  rcsid: str
-  email: str
-  password: str
-  major: Optional[str] = ""
-  graduate_year: Optional[str] = ""
-  description: Optional[str] = ""
-  discord: Optional[str] = ""
+    """Hold Information about Users."""
+    rcsid: str
+    email: str
+    password: str
+    major: Optional[str] = ""
+    graduate_year: Optional[str] = ""
+    description: Optional[str] = ""
+    discord: Optional[str] = ""
 
 class LoginInput(BaseModel):
-  """Hold Information about email verification."""
-  email: str
-  password: str
+	"""Hold Information about email verification."""
+	email: str
+	password: str
